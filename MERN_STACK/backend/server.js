1ab99362fb58c922d4f82108221f079c6dda03e7
King-A-19 <116169604+King-A-19@users.noreply.github.com>
2024-10-31 19:21:06 +0000

    Updated code with latest changes

---
+++ MERN_STACK/backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+// start app
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//route
+app.use('/api/workouts', workoutRoutes)
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT) 
+})
